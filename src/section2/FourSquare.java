dfc69bcc8f342f8c3ba95492751acdbb2da0de48
leoergen <66269675+leoergen@users.noreply.github.com>
2020-06-04 11:56:47 -0700

    four square (insane);

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -1,34 +1,46 @@
 package section2;
 
+import java.awt.Color;
+
 import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class FourSquare {
 	
 	// 2. Create a new Robot
-
+	Robot rob = new Robot();
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+		rob.setSpeed(30);
 		// 5. Set the pen width to 5
-
+		rob.setPenWidth(5);
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
-
+		for(int lines =0; lines <4; lines ++) {
 			// 7. Set the pen color to random
-	
+			rob.setRandomPenColor();
 			// 1. Call the drawSquare() method
-	//Leo39
+			drawSquare();
 			// 8. Turn the robot 90 degrees to the right
-
+			rob.turn(90);
+			}
 	}
 
 	
-	void drawSquare() {
+	void drawSquare(){
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
 		
-		
-	}
+		for(int lines =0; lines <4; lines ++) {
+			
+			
+			
+			rob.penDown();
+			rob.move(150);
+			rob.turn(90);
+		}	
+			
+	}	
+	
 
 	public static void main(String[] args) {
 		new FourSquare().go();
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,19 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	Robot Leo = new Robot();
+	Leo.setSpeed(30);
+	Leo.penDown();
+	Leo.setPenColor(Color.BLUE);
+	Leo.setPenWidth(40);
+	for(int lines =0; lines < 4; lines +=1 ) {
 	
-		
-		
+		Leo.move(150);
+		Leo.turn(90);
+	
+	}
+	Leo.move(100);
+	Leo.turn(-90);
+	//My Robot
 	}
 }
